fc3ed1cc75e242bb3cfb5bc7f3954cc5458d6ce2
Physik-Wombat <74726620+Physik-Wombat@users.noreply.github.com>
2020-12-08 14:24:52 +0100

    Create app.R

---
+++ app.R
@@ -0,0 +1,295 @@
+library(shiny)
+
+ui <- navbarPage(
+  strong("Wheatstonesche BrÃ¼ckenschaltung"),
+  
+  #
+  tabPanel(strong("Versuch 1"),
+           titlePanel(
+             h4(
+               "Nachweis, dass sich eine Spannung in der Potentiometerschaltung Ã¼ber die DrahtlÃ¤nge messen lÃ¤sst"
+             )
+           ),
+           
+           mainPanel(
+             tags$video(
+               src = "Versuch1.mp4",
+               width = "750px",
+               height = "520px",
+               type = "video/mp4",
+               controls = "controls"
+             )
+           )),
+  #
+  
+  tabPanel(
+    strong("Versuch 2"),
+    titlePanel(
+      h4(
+        "Bestimmung unbekannter WiderstÃ¤nde aus dem VerhÃ¤ltnis der DrahtlÃ¤ngen bei abgeglichener Wheatstonesche BrÃ¼cke"
+      )
+    ),
+    sidebarPanel(
+      radioButtons(
+        "wolo",
+        "",
+        choices = c("Widerstand Rx\u2081",
+                    "Widerstand Rx\u2082"),
+        selected = ""
+      ),
+      
+      
+      
+      conditionalPanel(condition = "input.wolo == 'Widerstand Rx\u2081'",
+                       selectInput(
+                         "dude", "",
+                         c("Messung 1A", "Messung 1B",
+                           "Messung 1C")
+                       )),
+      
+      conditionalPanel(condition = "input.wolo == 'Widerstand Rx\u2082'",
+                       selectInput(
+                         "rada", "",
+                         c("Messung 2A", "Messung 2B",
+                           "Messung 2C")
+                       )),
+      
+      
+      
+    ),
+    
+    conditionalPanel(
+      "input.wolo == 'Widerstand Rx\u2081'",
+      mainPanel(conditionalPanel("input.dude=='Messung 1A'",
+                                 
+                                 ((
+                                   tags$video(
+                                     src = "Rx1A.mp4",
+                                     width = "750px",
+                                     type = "video/mp4",
+                                     height = "520px",
+                                     controls = "controls"
+                                     
+                                   )
+                                 ))))
+    ),
+    
+    
+    
+    conditionalPanel(
+      "input.wolo == 'Widerstand Rx\u2081'",
+      mainPanel(conditionalPanel("input.dude=='Messung 1B'",
+                                 
+                                 ((
+                                   tags$video(
+                                     src = "Rx1B.mp4",
+                                     width = "750px",
+                                     type = "video/mp4",
+                                     height = "520px",
+                                     controls = "controls"
+                                     
+                                   )
+                                 ))))
+    ),
+    
+    conditionalPanel(
+      "input.wolo == 'Widerstand Rx\u2081'",
+      mainPanel(conditionalPanel("input.dude=='Messung 1C'",
+                                 
+                                 ((
+                                   tags$video(
+                                     src = "Rx1C.mp4",
+                                     width = "750px",
+                                     type = "video/mp4",
+                                     height = "520px",
+                                     controls = "controls"
+                                     
+                                   )
+                                 ))))
+    ),
+    
+    conditionalPanel(
+      "input.wolo == 'Widerstand Rx\u2082'",
+      mainPanel(conditionalPanel("input.rada=='Messung 2A'",
+                                 
+                                 ((
+                                   tags$video(
+                                     src = "Rx2A.mp4",
+                                     width = "750px",
+                                     type = "video/mp4",
+                                     height = "520px",
+                                     controls = "controls"
+                                     
+                                   )
+                                 ))))
+    ),
+    
+    conditionalPanel(
+      "input.wolo == 'Widerstand Rx\u2082'",
+      mainPanel(conditionalPanel("input.rada=='Messung 2B'",
+                                 
+                                 ((
+                                   tags$video(
+                                     src = "Rx2B.mp4",
+                                     width = "750px",
+                                     type = "video/mp4",
+                                     height = "520px",
+                                     controls = "controls"
+                                     
+                                   )
+                                 ))))
+    ),
+    
+    conditionalPanel(
+      "input.wolo == 'Widerstand Rx\u2082'",
+      mainPanel(conditionalPanel("input.rada=='Messung 2C'",
+                                 
+                                 ((
+                                   tags$video(
+                                     src = "Rx2C.mp4",
+                                     width = "750px",
+                                     type = "video/mp4",
+                                     height = "520px",
+                                     controls = "controls"
+                                     
+                                   )
+                                 ))))
+    )
+    
+  ),
+  
+  
+  
+  #
+  tabPanel(
+    strong("Versuch 3"),
+    titlePanel(
+      h4(
+        "Bestimmung unbekannter WiderstÃ¤nde aus der BrÃ¼ckenspannung bei nahezu abgeglichener Wheatstonesche BrÃ¼cke"
+      )
+    ),
+    br(),
+    
+    sidebarPanel(
+      "",
+      sliderInput(
+        inputId = "slider",
+        label = "Verschiedene WiderstÃ¤nde wÃ¤hlen:",
+        min = 0,
+        max = 297,
+        value = 0,
+        step = 33
+      )
+    ),
+    
+    
+    mainPanel(conditionalPanel("input.slider=='33'",
+                               
+                               ((
+                                 img(
+                                   src = "33Ohm.png",
+                                   height = 786,
+                                   width = 487
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='66'",
+                               
+                               ((
+                                 img(
+                                   src = "66Ohm.png",
+                                   height = 764,
+                                   width = 567
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='99'",
+                               
+                               ((
+                                 img(
+                                   src = "99Ohm.png",
+                                   height = 784,
+                                   width = 555
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='99'",
+                               
+                               ((
+                                 img(
+                                   src = "99Ohm.png",
+                                   height = 784,
+                                   width = 555
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='132'",
+                               
+                               ((
+                                 img(
+                                   src = "132Ohm.png",
+                                   height = 802,
+                                   width = 555
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='165'",
+                               
+                               ((
+                                 img(
+                                   src = "165Ohm.png",
+                                   height = 804,
+                                   width = 562
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='198'",
+                               
+                               ((
+                                 img(
+                                   src = "198Ohm.png",
+                                   height = 804,
+                                   width = 553
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='231'",
+                               
+                               ((
+                                 img(
+                                   src = "231Ohm.png",
+                                   height = 797,
+                                   width = 555
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='264'",
+                               
+                               ((
+                                 img(
+                                   src = "264Ohm.png",
+                                   height = 810,
+                                   width = 562
+                                 )
+                               )))),
+    
+    mainPanel(conditionalPanel("input.slider=='297'",
+                               
+                               ((
+                                 img(
+                                   src = "297Ohm.png",
+                                   height = 802,
+                                   width = 560
+                                 )
+                               )))),
+    
+    
+    #
+  )
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui = ui, server = server)
